9ba7a0c35971bde05c7b68fe74e7b76aa57d4a2c
im-nihar <niharsonawane@gmail.com>
2021-06-18 20:14:40 +0530

    [Basic project] Introduction to useReducers and context API

---
+++ src/components/MainHeader/Navigation.js
@@ -0,0 +1,63 @@
+import React, { useContext } from 'react';
+import AuthContext from '../../store/auth-context';
+
+import classes from './Navigation.module.css';
+
+const Navigation = () => {
+
+  // using useContext Hook
+  const ctx = useContext(AuthContext);
+
+  return (
+    <nav className={classes.nav}>
+      <ul>
+        {ctx.isLoggedIn && (
+          <li>
+            <a href="/">Users</a>
+          </li>
+        )}
+        {ctx.isLoggedIn && (
+          <li>
+            <a href="/">Admin</a>
+          </li>
+        )}
+        {ctx.isLoggedIn && (
+          <li>
+            <button onClick={ctx.onLogout}>Logout</button>
+          </li>
+        )}
+      </ul>
+    </nav>
+  );
+};
+
+// Using AuthContext
+// eslint-disable-next-line no-lone-blocks
+{/* <AuthContext.Consumer>
+      {(ctx) => {
+        // jsx code
+        return (
+          <nav className={classes.nav}>
+            <ul>
+              {ctx.isLoggedIn && (
+                <li>
+                  <a href="/">Users</a>
+                </li>
+              )}
+              {ctx.isLoggedIn && (
+                <li>
+                  <a href="/">Admin</a>
+                </li>
+              )}
+              {ctx.isLoggedIn && (
+                <li>
+                  <button onClick={props.onLogout}>Logout</button>
+                </li>
+              )}
+            </ul>
+          </nav>
+        )
+      }}
+    </AuthContext.Consumer> */}
+
+export default Navigation;
---
+++ src/components/UI/Input/Input.js
@@ -0,0 +1,40 @@
+import React, { useImperativeHandle, useRef } from "react";
+import classes from "./Input.module.css";
+
+const Input = React.forwardRef((props, ref) => {
+  console.log(props ,ref);
+  const inputRef = useRef();
+
+  const activate = () => {
+    inputRef.current.focus();
+  };
+
+  useImperativeHandle(ref, () => {
+    return {
+      // focus external name we set up in Login.js
+      focus: activate,
+    };
+  });
+
+  return (
+    <div>
+      <div
+        className={`${classes.control} ${
+          props.isValid === false ? classes.invalid : ""
+        }`}
+      >
+        <label htmlFor={props.id}>{props.label}</label>
+        <input
+          ref={inputRef}
+          type={props.type}
+          id={props.id}
+          value={props.value}
+          onChange={props.onChange}
+          onBlur={props.onBlur}
+        />
+      </div>
+    </div>
+  );
+});
+
+export default Input;
